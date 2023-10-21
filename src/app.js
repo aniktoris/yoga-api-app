3ef474cc87e8e6b26405c383a80e3977aafaff5b
oleno <es.moname@gmail.com>
2023-10-21 08:45:38 +0200

    Add categories on the screen

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 import { API_BASE_URL } from './constants.js';
 import { fetchJSON } from './api.js';
 import { initPage } from './pages/mainPage.js';
-import { fetchAndPopulateSanskritAsanas } from './api.js';
+import {
+  fetchAndPopulateSanskritAsanas,
+  fetchAndDisplayCategories,
+} from './api.js';
 
 const loadApp = async () => {
   initPage();
   try {
     const data = await fetchJSON(API_BASE_URL);
+    await fetchAndDisplayCategories(data);
     await fetchAndPopulateSanskritAsanas(data);
   } catch (error) {
     console.log(error);
